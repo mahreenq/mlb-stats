7773e8a4966c57f8e4b726d57d039e921315ebd0
Mahreen Qureshi <mahreenq@hotmail.com>
2019-09-07 19:24:38 -0400

    completed fielding chart

--- src/components/FieldingChart/FieldingChart.js
+++ src/components/FieldingChart/FieldingChart.js
@@ -1,66 +1,61 @@
-//import Typography from "@material-ui/core/Typography";
 import "./styles.css";
-
 import React, { Component } from "react";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
-
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 class FieldingChart extends Component {
-  // const classes = useStyles();
   render() {
     console.log("fielding stats", this.props.stats.splits[0].stat);
-    const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-    
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+    const stats = this.props.stats.splits[0].stat;
 
     return (
       <div className="rootTable">
-      <h1>FIELDING TABLE </h1>
-      <Paper className="paperTable">
-        <Table className="table" size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map(row => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">10</TableCell>
-                <TableCell align="right">20</TableCell>
-                <TableCell align="right">30</TableCell>
-                <TableCell align="right">40</TableCell>
+        {/* <h1>FIELDING TABLE </h1> */}
+        <Paper className="paperTable">
+          <Table className="table" size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>FIELDING</TableCell>
+
+                <TableCell align="center">Games</TableCell>
+                <TableCell align="center">Games Started</TableCell>
+                <TableCell align="center">Innings</TableCell>
+                <TableCell align="center">Opportunities</TableCell>
+                <TableCell align="center">Putouts</TableCell>
+                <TableCell align="center">Assists</TableCell>
+                <TableCell align="center">Errors</TableCell>
+                <TableCell align="center">Double Plays</TableCell>
+                <TableCell align="center">Fielding Percentage</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
-    </div>
- 
+            </TableHead>
+
+            <TableBody>
+              <TableRow>
+                <TableCell component="th" scope="row">
+                  2019
+                </TableCell>
+                <TableCell align="center">{stats.games}</TableCell>
+                <TableCell align="center">{stats.gamesStarted}</TableCell>
+                <TableCell align="center">{stats.innings}</TableCell>
+                <TableCell align="center">{stats.chances}</TableCell>
+                <TableCell align="center">{stats.putOuts}</TableCell>
+                <TableCell align="center">{stats.assists}</TableCell>
+                <TableCell align="center">{stats.errors}</TableCell>
+                <TableCell align="center">{stats.doublePlays}</TableCell>
+                <TableCell align="center">{stats.fielding}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
     );
   }
 }
 
 export default FieldingChart;
-
+// games, games started, innings, opportunities (chances), putouts
+//assists, errors, double plays, fielding percentage (FPCT)
